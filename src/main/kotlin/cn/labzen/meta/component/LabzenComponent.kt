5b03d5320b110660258cbbce88b2e9f737e288a3
Dean Zhao <rcarlosdasilva@qq.com>
2022-09-30 15:53:00 +0800

    chore: 简单整理一下

--- src/main/kotlin/cn/labzen/meta/component/LabzenComponent.kt
+++ src/main/kotlin/cn/labzen/meta/component/LabzenComponent.kt
@@ -2,9 +2,18 @@ package cn.labzen.meta.component
 
 interface LabzenComponent {
 
+  /**
+   * Labzen组件的唯一标识
+   */
   fun mark(): String
 
+  /**
+   * Labzen组件的包根路径
+   */
   fun packageBased(): String
 
+  /**
+   * Labzen组件简述
+   */
   fun description(): String
 }
