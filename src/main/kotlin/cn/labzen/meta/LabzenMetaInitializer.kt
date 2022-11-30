2f771c3420a08359caf798a2c41cd23b9b60a85b
Dean Zhao <rcarlosdasilva@qq.com>
2022-11-30 16:16:05 +0800

    refactor: 在spring中ApplicationContextInitializer的启动顺序修改

--- src/main/kotlin/cn/labzen/meta/LabzenMetaInitializer.kt
+++ src/main/kotlin/cn/labzen/meta/LabzenMetaInitializer.kt
@@ -3,6 +3,7 @@ package cn.labzen.meta
 import cn.labzen.meta.component.ComponentRecorder
 import cn.labzen.meta.configuration.ConfigurationReader
 import cn.labzen.meta.environment.EnvironmentCollector
+import cn.labzen.meta.spring.SpringApplicationContextInitializerOrder
 import cn.labzen.meta.system.SystemInformationCollector
 import org.springframework.context.ApplicationContextInitializer
 import org.springframework.context.ConfigurableApplicationContext
@@ -11,7 +12,7 @@ import org.springframework.core.Ordered
 class LabzenMetaInitializer : ApplicationContextInitializer<ConfigurableApplicationContext>, Ordered {
 
   override fun getOrder(): Int =
-    Int.MIN_VALUE + 10
+    SpringApplicationContextInitializerOrder.MODULE_META_INITIALIZER_ORDER
 
   override fun initialize(p0: ConfigurableApplicationContext) {
     ComponentRecorder.record()
